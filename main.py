8028d84e2925f614c22e23e0a69343afc7e63037
Bryan Travis Smith <Bryantravissmith@gmail.com>
2023-07-14 17:42:35 -0700

    successfully trained multiple models

--- main.py
+++ main.py
@@ -88,9 +88,19 @@ def go(config: DictConfig):
 
         if "data_split" in active_steps:
             ##################
-            # Implement here #
+            # data_split     #
             ##################
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                "main",
+                version='main',
+                parameters={
+                    "input": f"{config['main']['project_name']}/{config['etl']['preprocess_name']}:latest",
+                    "test_size": config["modeling"]["test_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                },
+            )
 
         if "train_random_forest" in active_steps:
 
@@ -99,14 +109,22 @@ def go(config: DictConfig):
             with open(rf_config, "w+") as fp:
                 json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
 
-            # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
-            # step
-
             ##################
             # Implement here #
             ##################
-
-            pass
+            _ = mlflow.run(
+                os.path.join(src_path, "train_random_forest"),
+                "main",
+                parameters={
+                    "trainval_artifact": f"{config['main']['project_name']}/{config['etl']['preprocess_name']}:latest",
+                    "rf_config": rf_config,
+                    "val_size": config["modeling"]["val_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify": config["modeling"]["stratify_by"],
+                    "max_tfidf_features": config["modeling"]["max_tfidf_features"],
+                    "output_artifact": "random_forest_model"
+                },
+            )
 
         if "test_regression_model" in active_steps:
 
